c20685e1f28cc2797a6fe27310bd0143f45dfa78
Kenneth Gjermstad <keanbegj@gmail.com>
2024-02-14 12:59:18 +0100

    Added Constructor to Product class

--- student/kegj002/projects/commerce/warehouse/src/Product.java
+++ student/kegj002/projects/commerce/warehouse/src/Product.java
@@ -2,6 +2,22 @@ package kegj002.projects.commerce.warehouse.src;
 
 public class Product
 {
+    //# Fields
     private String name;
     private double price;
+
+    //# Constructor
+    Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    //# Getters
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
