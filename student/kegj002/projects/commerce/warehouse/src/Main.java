2a0907273bd41c4ce1057c0fcec458e6d731fd2d
Kenneth Gjermstad <keanbegj@gmail.com>
2024-02-14 13:05:24 +0100

    Added another product

--- student/kegj002/projects/commerce/warehouse/src/Main.java
+++ student/kegj002/projects/commerce/warehouse/src/Main.java
@@ -13,6 +13,7 @@ public class Main
         Product cellphone = new Product("iPhone 15 Pro Max", 14995);
         Product charger = new Product("Belkin 20W charger", 599);
         Product cable = new Product("Belkin cable, 2m", 349);
+        Product energyDrink1 = new Product("Red Bull, 0,5l", 24.95);
 
         //# Flytt varer fra lageret til butikken
 
