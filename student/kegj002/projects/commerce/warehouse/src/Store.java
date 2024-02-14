11e5be8632115c0eb7d8e39e04bd85a6c7492d4e
Kenneth Gjermstad <keanbegj@gmail.com>
2024-02-14 13:01:50 +0100

    Added comments to mark Fields/Constructor/Getters

--- student/kegj002/projects/commerce/warehouse/src/Store.java
+++ student/kegj002/projects/commerce/warehouse/src/Store.java
@@ -2,14 +2,16 @@ package kegj002.projects.commerce.warehouse.src;
 
 public class Store
 {
+    //# Fields
     private Warehouse warehouse;
     private Inventory inventory = new Inventory();
 
-
+    //# Constructor
     Store() {
         this.warehouse = new Warehouse(this);
     }
 
+    //# Getters
     private Warehouse getWarehouse() {
         return this.warehouse;
     }
@@ -18,12 +20,11 @@ public class Store
         return this.getWarehouse().getInventory();
     }
 
-
+    //# Methods
     void moveProduct(Product product, int quantity) {
         // TODO flytt varen fra laget til butikken
         // må sjekke om dette er mulig først
     }
-
 
     double totalWarehouseValue() {
         // TODO regn ut verdien på alle varene på lager
@@ -34,7 +35,6 @@ public class Store
         // TODO regn ut verdien på alle varene ute i butikken
         return 0;
     }
-
 
     double totalValue() {
         return this.totalWarehouseValue() + this.totalStoreValue();
