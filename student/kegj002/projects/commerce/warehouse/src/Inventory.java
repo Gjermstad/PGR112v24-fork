bfb954846ce053fb7608c3de2480ae143689cade
Kenneth Gjermstad <keanbegj@gmail.com>
2024-02-26 11:27:18 +0100

    Added boolean for printing text in methods to console

--- student/kegj002/projects/commerce/warehouse/src/Inventory.java
+++ student/kegj002/projects/commerce/warehouse/src/Inventory.java
@@ -17,7 +17,8 @@ public class Inventory
 
     //# Methods
     public double totalValueOfProductsInInventory(boolean showEachProduct) {
-        System.out.println("*Calculating total value of products in the Warehouse...*");
+        if (showEachProduct) System.out.println("*Calculating total value of products in the Warehouse...*");
+
         double totalValue = 0;
 
         for (Product product : products.keySet()) {
@@ -34,15 +35,15 @@ public class Inventory
         return totalValue;
     }
 
-    public int totalNumberOfProductsInInventory() {
-        System.out.println("*Counting the stock in the Warehouse...*");
+    public int totalNumberOfProductsInInventory(boolean printText) {
+        if (printText) System.out.println("*Counting the stock in the Warehouse...*");
         double total = 0;
 
         for (Product product : products.keySet()) {
             total += products.get(product);
         }
 
-        System.out.println("There are " + (int) total + " products in the Warehouse.");
+        if (printText) System.out.println("There are " + (int) total + " products in the Warehouse.");
         return (int) total;
     }
 
@@ -50,35 +51,43 @@ public class Inventory
         products.put(product, quantity);
     }
 
-    void removeProduct(Product product, int quantity) {
+    void removeProduct(Product product, int quantity, boolean printText) {
 
         for (Product currentProduct : products.keySet()) {
 
             if (currentProduct.equals(product)) {
 
-                System.out.println("*Updating inventory...*");
-                System.out.println("We have " + this.getAmountOfProductInStock(product) + " of the product " + product + " in the Warehouse inventory.");
+                if (printText) System.out.println("*Updating inventory...*");
+                if (printText) System.out.println("We have " + this.getAmountOfProductInStock(product) + " of the product " + product + " in the Warehouse inventory.");
 
                 if (products.get(product) > quantity) {
-                    System.out.println("We remove " + quantity + " of the product " + product + " from the Warehouse inventory.");
+                    if (printText) System.out.println("We remove " + quantity + " of the product " + product + " from the Warehouse inventory.");
 
                     this.products.put(product, products.get(product) - quantity);
 
-                    System.out.println("We now have " + products.get(product) + " left of " + product + " in the Warehouse.");
+                    if (printText) System.out.println("We now have " + products.get(product) + " left of " + product + " in the Warehouse.");
 
                 } else if (products.get(product) == quantity) {
 
                     this.products.remove(product);
-                    System.out.println("We removed all of " + product + " from the Warehouse.");
+                    if (printText) System.out.println("We removed all of " + product + " from the Warehouse.");
 
                 } else {
                     System.out.println("Insufficient Stock!");
-                    System.out.println("Only " + products.get(product) + " available of " + product);
+                    System.out.println("Only " + products.get(product) + " available of " + product + ".");
                 }
 
             }
 
         }
 
+    }
+
+    @Override
+    public String toString() {
+        return "Inventory {" +
+                "number of unique products = " + products.size() +
+                " / total stock = " + this.totalNumberOfProductsInInventory(false) +
+                '}';
     }
 }
