2dc8097cc7aea1cdb31ff4313efcbe30a5383615
Kenneth Gjermstad <keanbegj@gmail.com>
2024-03-06 12:14:52 +0100

    Put products into customers cart

--- student/kegj002/projects/commerce/store/src/Main.java
+++ student/kegj002/projects/commerce/store/src/Main.java
@@ -14,16 +14,27 @@ public class Main
 
         Product apple = new Product("Apple", 10);
         Product banana = new Product("Banana", 8);
-        Product groundMeat = new Product("Ground Beef", 79);
-        Product shreddedCheese = new Product("Shredded Cheese", 99);
-        Product tortilla = new Product("Tortillas Wholegrain", 20);
+        Product groundMeat = new Product("Ground Beef, 400g", 79);
+        Product shreddedCheese = new Product("Shredded Cheese, 1kg", 99);
+        Product tortilla = new Product("Tortillas Wholegrain", 4);
         Product salsa = new Product("Salsa sauce, medium", 17);
-        Product pineapple = new Product("Pineapple in pieces", 15);
+        Product pineapple = new Product("Pineapple in pieces, 1 can", 15);
         Product lettuce = new Product("Head of lettuce", 29);
         Product tomato = new Product("Tomato", 5);
-        Product cucumber = new Product("Cucmber", 19);
+        Product cucumber = new Product("Cucumber", 19);
 
         //# 2) Add products to customer's shopping cart
+
+        customer.putProductInCart(tortilla, 5);
+        customer.putProductInCart(salsa, 1);
+        customer.putProductInCart(groundMeat, 2);
+        customer.putProductInCart(shreddedCheese, 1);
+        customer.putProductInCart(tomato, 6);
+        customer.putProductInCart(cucumber, 2);
+        customer.putProductInCart(pineapple, 2);
+        customer.putProductInCart(lettuce, 2);
+        customer.putProductInCart(apple, 4);
+        customer.putProductInCart(banana, 3);
 
         //# 3) Process the buying of products via the cash register
         boolean success = store.getCashRegister().processCustomer(customer);
