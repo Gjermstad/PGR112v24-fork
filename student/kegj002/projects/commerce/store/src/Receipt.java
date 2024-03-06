95254337e4cf2f3436f13966ca5419ccb1e07479
Kenneth Gjermstad <keanbegj@gmail.com>
2024-03-06 14:16:38 +0100

    Added field and Getter for receiptNumber

--- student/kegj002/projects/commerce/store/src/Receipt.java
+++ student/kegj002/projects/commerce/store/src/Receipt.java
@@ -8,16 +8,24 @@ public class Receipt
     //# Fields
     private final HashMap<Product, Integer> products;
 
+    private static int counter = 0;
+    private final int receiptNumber;
+
 
     //# Constructors
     Receipt(HashMap<Product, Integer> products) {
         this.products = products;
+        this.receiptNumber = Receipt.counter++;
     }
 
 
     //# Getter-methods
     HashMap<Product, Integer> getProducts() {
         return this.products;
+    }
+
+    public int getReceiptNumber() {
+        return this.receiptNumber;
     }
 
     //# Methods
