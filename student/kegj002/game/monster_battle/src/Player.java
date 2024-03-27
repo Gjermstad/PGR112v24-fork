1bf3eeb2f02aa3aa92af8647f05a8c487a9b0bb5
Kenneth A.B.G <79542593+Gjermstad@users.noreply.github.com>
2024-03-27 10:26:07 +0100

    Added toString

--- student/kegj002/game/monster_battle/src/Player.java
+++ student/kegj002/game/monster_battle/src/Player.java
@@ -10,4 +10,9 @@ public class Player extends Entity
     void entityDied() {
         System.out.println("The Player dies!");
     }
+
+    @Override
+    public String toString() {
+        return "Player";
+    }
 }
